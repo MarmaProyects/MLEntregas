e6c72962fdc2ee1fe945f65f0ed56aa34952c68d
¨Gaston <gastonaguilera@gmail.com>
2023-08-23 20:31:13 -0300

    [CreacionClases] Creacion de las clases tarifa, localidad, direccion y seccion

---
+++ src/logica/clases/Tarifa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.clases;
+
+/**
+ *
+ * @author MarmaduX
+ */
+public class Tarifa {
+    private float precio;
+    private String nombre;
+    private int idTarifa;
+         
+    
+    
+}
+}
