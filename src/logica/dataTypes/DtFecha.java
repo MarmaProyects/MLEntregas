244f898c4c739e089e4803993d06394efa62465d
leonardoCoronel4 <129887408+leonardoCoronel4@users.noreply.github.com>
2023-08-23 22:56:24 -0300

    conneccion de las clases (#10)

--- src/logica/dataTypes/DtFecha.java
+++ src/logica/dataTypes/DtFecha.java
@@ -10,4 +10,54 @@ package logica.dataTypes;
  */
 public class DtFecha {
     private int minuto, hora, dia, mes, anio;
+
+    public DtFecha(int minuto, int hora, int dia, int mes, int anio) {
+        this.minuto = minuto;
+        this.hora = hora;
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
 }
