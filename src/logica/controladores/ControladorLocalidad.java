c8d21aefc09eb974e793c3808bd2ec6410abe08f
¨Gaston <gastonaguilera@gmail.com>
2023-08-26 16:07:08 -0300

    [InterfaceControllers] creacion de controladores e interfaces

---
+++ src/logica/controladores/ControladorLocalidad.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.controladores;
+
+import logica.interfaces.IProximidad;
+
+/**
+ *
+ * @author MarmaduX
+ */
+public class ControladorLocalidad implements IProximidad {
+    
+}
