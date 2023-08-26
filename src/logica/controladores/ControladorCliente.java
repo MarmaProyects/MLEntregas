07c7d15b6946ff7d7645952f8359ed4d24c30e39
Gastón Aguilera <43421678+MarmaduX@users.noreply.github.com>
2023-08-26 16:13:29 -0300

    [InterfaceControllers] creacion de controladores e interfaces (#12)

---
+++ src/logica/controladores/ControladorCliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica.controladores;
+
+import logica.interfaces.IAdministracion;
+
+/**
+ *
+ * @author MarmaduX
+ */
+public class ControladorCliente implements IAdministracion {
+
+}
